6ec275fe14703b6355c08a7a410faf40356cc7f1
Messias Junior <messias.junior@getchip.uk>
2023-03-17 19:53:03 +0000

    test: SplashLoadingIndicator

--- src/commonMain/kotlin/appoutlet/gameoutlet/feature/splash/composable/SplashLoadingIndicator.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/feature/splash/composable/SplashLoadingIndicator.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
 import appoutlet.gameoutlet.core.translation.i18n
 import appoutlet.gameoutlet.core.ui.GameOutletTheme
 import appoutlet.gameoutlet.core.ui.spacing
@@ -17,7 +19,7 @@ import appoutlet.gameoutlet.core.ui.spacing
 @Composable
 fun SplashLoadingIndicator(modifier: Modifier = Modifier) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        LinearProgressIndicator()
+        LinearProgressIndicator(modifier = Modifier.semantics { testTag = "splashLoadingIndicator" })
         Spacer(modifier = Modifier.height(MaterialTheme.spacing.small))
         Text(i18n.tr("Loading"), style = MaterialTheme.typography.bodyMedium)
     }
