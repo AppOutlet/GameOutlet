766774618b7bb8dd30074aa3017f39ce3248f863
Messias Junior <messias.junior@getchip.uk>
2023-05-04 18:37:31 +0100

    update lib update

--- src/commonMain/kotlin/appoutlet/gameoutlet/feature/latestdeals/composable/Deal.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/feature/latestdeals/composable/Deal.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
