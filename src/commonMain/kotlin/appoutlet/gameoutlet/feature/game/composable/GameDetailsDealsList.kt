63b9fe2614d9b833102485592441929e57862846
Messias Junior <messias.junior@getchip.uk>
2023-05-10 17:56:51 +0100

    test: fix GameDetailsDealsListKtTest

--- src/commonMain/kotlin/appoutlet/gameoutlet/feature/game/composable/GameDetailsDealsList.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/feature/game/composable/GameDetailsDealsList.kt
@@ -1,15 +1,9 @@
 package appoutlet.gameoutlet.feature.game.composable
 
 import androidx.compose.animation.core.tween
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.Card
@@ -20,10 +14,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import appoutlet.gameoutlet.core.translation.i18n
+import appoutlet.gameoutlet.core.ui.GameOutletTheme
 import appoutlet.gameoutlet.core.ui.spacing
+import appoutlet.gameoutlet.feature.game.GameDealStoreUiModel
 import appoutlet.gameoutlet.feature.game.GameDealUiModel
 import appoutlet.gameoutlet.feature.game.GameInputEvent
 import appoutlet.gameoutlet.feature.game.GameUiModel
@@ -81,9 +79,12 @@ fun GameDetailsDealsList(uiState: GameUiModel, onInputEvent: (GameInputEvent) ->
 @Composable
 private fun Deal(item: GameDealUiModel, onInputEvent: (GameInputEvent) -> Unit, modifier: Modifier = Modifier) {
     Row(
-        modifier = modifier.fillMaxWidth().clickable {
-            onInputEvent(GameInputEvent.DealClicked(item))
-        },
+        modifier = modifier.fillMaxWidth()
+            .clickable {
+                onInputEvent(GameInputEvent.DealClicked(item))
+            }.semantics {
+                testTag = "deal ${item.id}"
+            },
         verticalAlignment = Alignment.CenterVertically
     ) {
         KamelImage(
@@ -91,6 +92,11 @@ private fun Deal(item: GameDealUiModel, onInputEvent: (GameInputEvent) -> Unit, 
             resource = lazyPainterResource(item.store.icon),
             contentDescription = null,
             animationSpec = tween(),
+            onLoading = {
+                Box(
+                    modifier = Modifier.padding(MaterialTheme.spacing.medium).size(64.dp),
+                ) { }
+            }
         )
 
         Text(modifier = Modifier.weight(1f), text = item.store.name, style = MaterialTheme.typography.titleLarge)
@@ -113,5 +119,30 @@ private fun Deal(item: GameDealUiModel, onInputEvent: (GameInputEvent) -> Unit, 
                 )
             }
         }
+    }
+}
+
+
+@Composable
+@Preview
+private fun GameDetailsDealsListPreview() {
+    val gameUiModel = GameUiModel(
+        title = "Game",
+        image = "",
+        deals = listOf(
+            GameDealUiModel(
+                id = "123",
+                store = GameDealStoreUiModel(
+                    name = "Store",
+                    icon = ""
+                ),
+                salePrice = "123",
+                normalPrice = "123",
+                showNormalPrice = true
+            )
+        )
+    )
+    GameOutletTheme {
+        GameDetailsDealsList(gameUiModel, {})
     }
 }
