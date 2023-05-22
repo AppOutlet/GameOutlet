8b7fbe87b5883852ee587031f4d02cf1e3b40c07
Messias Junior <messias.junior@getchip.uk>
2023-05-22 22:01:39 +0100

    feat: update saved games automatically

--- src/commonMain/kotlin/appoutlet/gameoutlet/feature/wishlist/WishlistViewModel.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/feature/wishlist/WishlistViewModel.kt
@@ -1,20 +1,24 @@
 package appoutlet.gameoutlet.feature.wishlist
 
+import appoutlet.gameoutlet.core.database.GameQueries
 import appoutlet.gameoutlet.feature.common.ViewModel
 import appoutlet.gameoutlet.feature.game.GameNavArgs
 import appoutlet.gameoutlet.feature.game.GameViewProvider
 import appoutlet.gameoutlet.feature.home.composable.GameSearchTab
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
 
 class WishlistViewModel(
     private val wishlistOrchestrator: WishlistOrchestrator,
     private val wishlistUiStateMapper: WishlistUiStateMapper,
     private val gameViewProvider: GameViewProvider,
+    private val gameQueries: GameQueries,
 ) : ViewModel<WishlistUiState, WishlistInputEvent>(initialState = WishlistUiState.Idle) {
+    init {
+        // remove when sqlite was updated
+        gameQueries.findAll().addListener {
+            loadSavedGames()
+        }
+    }
+
     override fun onInputEvent(inputEvent: WishlistInputEvent) {
         when (inputEvent) {
             WishlistInputEvent.Load -> loadSavedGames()
@@ -26,14 +30,9 @@ class WishlistViewModel(
     }
 
     private fun loadSavedGames() {
-        wishlistOrchestrator.findAll()
-            .map { wishlistUiStateMapper(it) }
-            .onStart { mutableUiState.value = WishlistUiState.Loading }
-            .catch { mutableUiState.value = WishlistUiState.Error }
-            .onEach {
-                mutableUiState.value = WishlistUiState.Loaded(it)
-            }
-            .launchIn(viewModelScope)
+        val games = wishlistOrchestrator.findAll()
+        val uiModels = wishlistUiStateMapper(games)
+        mutableUiState.value = WishlistUiState.Loaded(uiModels)
     }
 
     private fun navigateToGameDetail(gameUiModel: WishlistGameUiModel) {
--- src/commonMain/kotlin/appoutlet/gameoutlet/feature/wishlist/composable/WishlistEmptyList.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/feature/wishlist/composable/WishlistEmptyList.kt
@@ -9,12 +9,15 @@ import appoutlet.gameoutlet.core.translation.i18n
 import appoutlet.gameoutlet.feature.common.composable.Error
 import appoutlet.gameoutlet.feature.wishlist.WishlistInputEvent
 
+@Suppress("MaxLineLength")
 @Composable
 fun WishlistEmptyList(onInputEvent: (WishlistInputEvent) -> Unit, modifier: Modifier = Modifier) {
     Error(
         modifier = modifier.fillMaxHeight().widthIn(max = 500.dp),
         title = i18n.tr("You didn't save any game yet"),
-        message = i18n.tr("Your saved games will appear here. See the latest deals or search by your favourite game and then click on the favorite button."),
+        message = i18n.tr(
+            "Your saved games will appear here. See the latest deals or search by your favourite game and then click on the favorite button."
+        ),
         buttonText = i18n.tr("Take me to search screen"),
         onTryAgain = { onInputEvent(WishlistInputEvent.GoToSearch) },
     )
