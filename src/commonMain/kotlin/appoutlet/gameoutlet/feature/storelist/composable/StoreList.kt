784ea372f7ff4186e1f103493c46e102605eff0d
Messias Junior <messias.junior@getchip.uk>
2023-05-30 19:50:52 +0100

    feat: show stores name

--- src/commonMain/kotlin/appoutlet/gameoutlet/feature/storelist/composable/StoreList.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/feature/storelist/composable/StoreList.kt
@@ -1,9 +1,19 @@
 package appoutlet.gameoutlet.feature.storelist.composable
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import appoutlet.gameoutlet.feature.storelist.StoreListInputEvent
 import appoutlet.gameoutlet.feature.storelist.StoreListUiState
+import appoutlet.gameoutlet.feature.storelist.StoreUiModel
 
 @Composable
 fun StoreList(
@@ -11,5 +21,28 @@ fun StoreList(
     onInputEvent: (StoreListInputEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(uiState.stores) { store ->
+            Store(
+                uiModel = store,
+                onInputEvent = onInputEvent
+            )
+        }
+    }
+}
 
+@Composable
+private fun Store(
+    uiModel: StoreUiModel,
+    onInputEvent: (StoreListInputEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier.widthIn(maxOf(512.dp))) {
+        Row(modifier.fillMaxWidth()) {
+            Text(uiModel.name)
+        }
+    }
 }
