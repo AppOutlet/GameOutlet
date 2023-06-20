37ba8ee961da57c28cc730de6bd5c727039f2350
AppOutlet team <team.appoutlet@gmail.com>
2023-06-20 18:47:01 +0000

    chore: bump to version 1.3.2

--- src/commonMain/kotlin/appoutlet/gameoutlet/feature/about/composable/AboutScreen.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/feature/about/composable/AboutScreen.kt
@@ -35,7 +35,7 @@ import appoutlet.gameoutlet.core.ui.spacing
 import appoutlet.gameoutlet.feature.about.AboutInputEvent
 import appoutlet.gameoutlet.feature.about.AboutUiState
 
-const val VERSION = "1.3.1"
+const val VERSION = "1.3.2"
 
 @Composable
 fun AboutScreen(
