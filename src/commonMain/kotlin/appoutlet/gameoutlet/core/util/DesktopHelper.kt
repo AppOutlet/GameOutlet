8f679f2a10b8f149b0a91c0d028506b0b41e60f3
Messias <messiaslima.03@gmail.com>
2023-07-03 09:42:18 +0000

    fix: use xdg open as fallback to open links

--- src/commonMain/kotlin/appoutlet/gameoutlet/core/util/DesktopHelper.kt
+++ src/commonMain/kotlin/appoutlet/gameoutlet/core/util/DesktopHelper.kt
@@ -1,10 +1,18 @@
 package appoutlet.gameoutlet.core.util
 
+import io.github.aakira.napier.Napier
 import java.awt.Desktop
 import java.net.URI
 
 class DesktopHelper {
     fun openLink(url: String) {
-        Desktop.getDesktop().browse(URI(url))
+        val desktop = Desktop.getDesktop()
+
+        if (desktop.isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(URI(url))
+        } else {
+            Napier.w("Desktop utils not supported. Trying with xdg-open")
+            Runtime.getRuntime().exec(arrayOf("xdg-open", url))
+        }
     }
 }
